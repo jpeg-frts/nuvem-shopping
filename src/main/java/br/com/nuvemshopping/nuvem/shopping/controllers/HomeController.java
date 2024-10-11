357a0092457e7e3d7464534519cf963b6f95f963
joão freitas <jpfserrao111@gmail.com>
2024-10-11 00:00:26 -0300

    retirei linhas sem uso

--- src/main/java/br/com/nuvemshopping/nuvem/shopping/controllers/HomeController.java
+++ src/main/java/br/com/nuvemshopping/nuvem/shopping/controllers/HomeController.java
@@ -1,10 +1,7 @@
 package br.com.nuvemshopping.nuvem.shopping.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
